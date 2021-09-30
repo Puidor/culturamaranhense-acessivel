068e829b634d4122dfb9faa512565df42b4ffdb9
Puidor <c.adriano.silva@hotmail.com>
2021-09-30 10:50:47 -0300

    Comentarios script

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const Modal = {
+  // Função para abrir o Modal
   open() {
     document.querySelector(".modal").classList.add("menu-active");
   },
+  // Função para fechar o Modal
   close() {
     document.querySelector(".modal").classList.remove("menu-active");
   },
